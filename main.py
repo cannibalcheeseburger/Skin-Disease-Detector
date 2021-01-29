6d1937b092af52219094c2a482094c8ac04bbe4e
Kashish srivastava <44269697+cannibalcheeseburger@users.noreply.github.com>
2021-01-29 23:30:06 +0530

    introduced a fuckall api

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from fastapi import UploadFile,File
 import uvicorn
+from random import randint
+from src import const
+import os
 
 app = FastAPI()
 
@@ -12,3 +15,9 @@ async def hello():
 async def predict(file: UploadFile = File(...)):
     return {"all":"right"}
 
+@app.get('/predict/random')
+async def predict_random():
+    #eturn os.getcwd()
+    with open(const.diagnosis_dir+ const.diseases[randint(0,11)],'r',encoding='utf-8') as f:
+        diagnosis = f.read()
+    return {str(randint(0,100))+'%':diagnosis}
