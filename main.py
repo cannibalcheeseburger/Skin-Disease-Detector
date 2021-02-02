048c5548f192aaf64b82ce337419db06ea787ee9
Akash Rana <akashrana20022000@gmail.com>
2021-02-02 20:27:34 +0530

    frontend 2/10

--- main.py
+++ main.py
@@ -1,14 +1,44 @@
-from fastapi import FastAPI
-from fastapi import UploadFile,File
+from fastapi import FastAPI, Request, UploadFile, File
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 import uvicorn
+from random import randint
+from src import const, preprocess
+import os
+import shutil
+from pathlib import Path
+import json
+import time
+
+templates = Jinja2Templates(directory="./templates")
 
 app = FastAPI()
 
+app.mount(
+    "/static",
+    StaticFiles(directory=Path(__file__).parent.parent.absolute() / "static"),
+    name="static",
+)
+
+
 @app.get('/')
-async def hello():
-    return {"hello": "there"}
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
 
 @app.post('/predict')
-async def predict(file: UploadFile = File(...)):
-    return {"all":"right"}
+async def predict(image: UploadFile = File(...)):
+    temp_file = save_to_disk(image, path="temp", save_as='temp')
+    result = preprocess.predict(temp_file)
+    with open(const.diagnosis_dir + const.diseases[result]+".json", 'r', encoding='utf-8') as f:
+    	diagnosis = json.load(f)
+    return diagnosis
+   
 
+
+def save_to_disk(uploadedfile, path='.', save_as='default'):
+    extension = os.path.splitext(uploadedfile.filename)[-1]
+    temp_file = os.path.join(path, save_as+extension)
+    with open(temp_file, 'wb') as buffer:
+        shutil.copyfileobj(uploadedfile.file, buffer)
+    return temp_file
---
+++ src/preprocess.py
@@ -0,0 +1,12 @@
+from tensorflow.keras.preprocessing import image
+import numpy as np
+import tensorflow.keras.models as models
+
+
+def predict(temp_file):
+    test_image = image.load_img(temp_file, target_size=(256, 256))
+    test_image = image.img_to_array(test_image)
+    test_image = np.expand_dims(test_image, axis=0)
+    model = models.load_model('best_model.h5')
+    result = model.predict(test_image)
+    return np.argmax(result)
